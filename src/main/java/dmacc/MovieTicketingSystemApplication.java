6772bece0069dd0c89af321e5d532c999e09b2d2
sprhone <sprhone@users.noreply.github.com>
2021-04-15 10:11:37 -0500

    Modified web files, application.properties, added test data
    Modified .jsp files to be .html compliant (note in /viewMovies.html still needs updated).
    This way we can use Thymeleaf within the Spring MVC model.
    
    Modified application.properties to use local MySQL database for testing. (AWS connection is just commented out so we can turn it back on quickly.)
    
    Added link to .css on index page.
    
    Many changes to Auditorium, Movies, and system application to add testing generators for movies.
    
    Through testing, current code shows index and viewMovies code correctly (minus showing the movies (see note in code)) and properly persists in the database.

--- src/main/java/dmacc/MovieTicketingSystemApplication.java
+++ src/main/java/dmacc/MovieTicketingSystemApplication.java
@@ -11,7 +11,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.support.AbstractApplicationContext;
 
 import dmacc.controller.BeanConfiguration;
+import dmacc.beans.Auditorium;
+import dmacc.beans.Movies;
 import dmacc.beans.ProgramUsers;
+import dmacc.repository.MoviesRepository;
 import dmacc.repository.ProgramUsersRepository;
 
 @SpringBootApplication
@@ -23,26 +26,48 @@ public class MovieTicketingSystemApplication implements CommandLineRunner {
 	}
 	
 	@Autowired
-	ProgramUsersRepository repo;
+	ProgramUsersRepository pRepo;
+	
+	@Autowired
+	MoviesRepository mRepo;
 	
 	@Override
 	public void run(String...args) throws Exception {
 		ApplicationContext appContext = new AnnotationConfigApplicationContext(BeanConfiguration.class);
 		ProgramUsers pu = appContext.getBean("programUsers", ProgramUsers.class);
 		pu.setUserName("JimmyBobs");
-		repo.save(pu);
+		pRepo.save(pu);
 		
 		ProgramUsers d = new ProgramUsers("Customer", "Base", "Jimmy Bobs");
-		repo.save(d);
-		List<ProgramUsers> allMyUsers = repo.findAll();
+		pRepo.save(d);
+		List<ProgramUsers> allMyUsers = pRepo.findAll();
 		for(ProgramUsers people: allMyUsers) {
 			System.out.println(people.toString());
 		}
 	
 		
 		((AbstractApplicationContext) appContext).close();
-	}
+		
+		
+		
+		
+		ApplicationContext appContext1 = new AnnotationConfigApplicationContext(BeanConfiguration.class);
+		Movies m = appContext1.getBean("movies", Movies.class);
+		m.setMovieName("The Princess bride");
+		mRepo.save(m);
+		 
+		
+		Auditorium aud = new Auditorium(15, 22, "Big Room");
+	  
+		Movies v = new Movies("Monty Python and the Holy Grail", "PG", 91, aud);
+		mRepo.save(v);
+		List<Movies> allMyMovies = mRepo.findAll();
+		for(Movies mov: allMyMovies) {
+			System.out.println(mov.toString());
+		}
 	
-
+		
+		 ((AbstractApplicationContext) appContext1).close(); 
+	}
 }
 
